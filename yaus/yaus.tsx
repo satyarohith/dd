6ad065a3d8aa87e2b41ee343f0f25257522718e6
Satya Rohith <me@satyarohith.com>
2021-02-01 23:48:50 +0530

    refactor(yaus): move things closer

--- yaus/yaus.tsx
+++ yaus/yaus.tsx
@@ -1,3 +1,57 @@
+import {
+  json,
+  PathParams,
+  serve,
+  validateRequest,
+} from "https://deno.land/x/sift@0.1.3/mod.ts";
+import { nanoid } from "https://cdn.esm.sh/v14/nanoid@3.1.20/esnext/nanoid.js";
+
+serve({
+  "/:code": handleCodeReqs,
+  "/api/create": handleCreate,
+});
+
+/** API Handlers -- start. */
+async function handleCreate(request: Request) {
+  const { error } = await validateRequest(request, {
+    GET: {
+      params: ["url"],
+    },
+  });
+  if (error) {
+    return json({ error: error.message }, { status: error.status });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code") ?? nanoid(6);
+  const url = searchParams.get("url")!;
+  const resCode = await findCode(url);
+  if (resCode) {
+    return json({ code: resCode, url }, { status: 200 });
+  }
+
+  return json(await add(url, code), { status: 201 });
+}
+
+async function handleCodeReqs(request: Request, params?: PathParams) {
+  const { error } = await validateRequest(request, { GET: {} });
+  if (error) {
+    return json({ error: error.message }, { status: error.status });
+  }
+
+  const { code = "" } = params as { code: string };
+  if (code) {
+    const url = await findUrl(code);
+    if (url) {
+      return Response.redirect(url, 307);
+    }
+  }
+
+  return json({ error: "page not found" }, { status: 404 });
+}
+/** API Handlers -- end. */
+
+/** Fauna related code -- start. */
 /** Stores the code as key and url as value. */
 const codeKV: { [key: string]: string } = {};
 /** Stores the url as key and code as value. */
@@ -15,7 +69,7 @@ async function findUrl(code: string): Promise<string | null> {
     return codeKV[code];
   }
 
-  const { data } = (await queryFuana(findUrlQuery, { code })) as {
+  const { data } = (await queryFauna(findUrlQuery, { code })) as {
     data: { findUrlByCode: { url: string } };
   };
   if (data?.findUrlByCode?.url) {
@@ -38,7 +92,7 @@ async function findCode(url: string): Promise<string | null> {
     return urlKV[url];
   }
 
-  const { data } = (await queryFuana(findCodeQuery, { url })) as {
+  const { data } = (await queryFauna(findCodeQuery, { url })) as {
     data: { findCodeByUrl: { code: string } };
   };
   if (data?.findCodeByUrl?.code) {
@@ -61,13 +115,14 @@ async function add(
   url: string,
   code: string,
 ): Promise<{ code: string; url: string }> {
-  const { data } = (await queryFuana(createLinkQuery, { url, code })) as {
-    data: { url: string; code: string };
-  };
+  const { data: { createLink } } =
+    (await queryFauna(createLinkQuery, { url, code })) as {
+      data: { createLink: { url: string; code: string } };
+    };
 
-  codeKV[code] = data?.url;
-  urlKV[url] = data?.code;
-  return data;
+  codeKV[code] = createLink?.url;
+  urlKV[url] = createLink?.code;
+  return createLink;
 }
 
 /** This is a wrapper function to get formatting and syntax work for graphql in vscode. */
@@ -75,7 +130,7 @@ function gql(template: TemplateStringsArray) {
   return template.join("");
 }
 
-async function queryFuana(
+async function queryFauna(
   query: string,
   variables: { [key: string]: unknown },
 ): Promise<{
@@ -110,5 +165,3 @@ async function queryFuana(
     return { error };
   }
 }
-
-export { add, findCode, findUrl };
